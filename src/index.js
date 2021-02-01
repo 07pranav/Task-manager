a8ea3526881c4f697ba9342ff410db44a1ac71c4
07pranav <07pranav@users.noreply.github.com>
2021-02-01 16:35:39 +0530

    little change

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
-app.use(userRouter)
 app.use(taskRouter)
+app.use(userRouter)
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
